a919bb877c9923dec16213f3576d3e9a30b996d1
jusoto <jusoto@gmail.com>
2018-03-21 11:41:02 -0400

    Applied CheckStyles. An fixed Unix end of line format.

--- src/main/java/org/aitesting/microservices/configservice/ConfigServiceApplication.java
+++ src/main/java/org/aitesting/microservices/configservice/ConfigServiceApplication.java
@@ -9,8 +9,8 @@ import org.springframework.cloud.netflix.eureka.EnableEurekaClient;
 @EnableEurekaClient
 @EnableConfigServer
 public class ConfigServiceApplication {
-
-	public static void main(String[] args) {
-		SpringApplication.run(ConfigServiceApplication.class, args);
-	}
+    
+    public static void main(String[] args) {
+        SpringApplication.run(ConfigServiceApplication.class, args);
+    }
 }
